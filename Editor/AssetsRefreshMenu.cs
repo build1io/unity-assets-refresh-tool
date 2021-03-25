15b33b6ba52432dde0e70e178327a84f4b8488b6
George Markosian <georgemarkosian@gmail.com>
2021-03-25 20:16:43 +0200

    Initial commit.

---
+++ Editor/AssetsRefreshMenu.cs
@@ -0,0 +1,60 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace Build1.AssetsRefreshTool.Editor
+{
+    [InitializeOnLoad]
+    internal static class AssetsRefreshMenu
+    {
+        private const string AutoRefreshEnabledMenuItem = "Build1/Tools/Assets Refresh/Auto Refresh";
+        private const string RefreshOnPlayMenuItem      = "Build1/Tools/Assets Refresh/Refresh on Play";
+        private const string InfoMenuItem               = "Build1/Tools/Assets Refresh/Info";
+
+        static AssetsRefreshMenu()
+        {
+            EditorApplication.delayCall += UpdateMenu; 
+        }
+
+        [MenuItem(AutoRefreshEnabledMenuItem, false, 1000)]
+        public static void AutoRefresh()
+        {
+            if (AssetsRefreshTool.SetEnabled(false))
+                UpdateMenu();
+        }
+
+        [MenuItem(RefreshOnPlayMenuItem, false, 1001)]
+        public static void RefreshOnPlay()
+        {
+            if (AssetsRefreshTool.SetEnabled(true))
+                UpdateMenu();
+        }
+
+        [MenuItem(InfoMenuItem, false, 1050)]
+        public static void Info()
+        {
+            EditorUtility.DisplayDialog("Assets Refresh Tool",
+                                        "This is a short instruction for Assets Refresh Tool.\n\n" +
+                                        "Auto Refresh - is the default Unity's Auto Refresh feature.\n\n" +
+                                        "Refresh on Play - disables the Auto Refresh and performs refresh on play.\n\n" +
+                                        "With Refresh on Play enabled Unity Editor will not update project assets/metadata. You'll have to refresh it manually by using your OS's hot key.\n\n" +
+                                        "Refresh on Play saves a lot of time when you work mostly on the code. Code changes and window switching will not stop play and will not trigger scripts recompilation when you don't need it.\n\n" +
+                                        "Good luck!",
+                                        "Got it!");
+            
+        }
+        
+        /*
+         * Private.
+         */
+
+        private static void UpdateMenu()
+        {
+            var enabled = AssetsRefreshTool.GetEnabled();
+            Menu.SetChecked(AutoRefreshEnabledMenuItem, !enabled);
+            Menu.SetChecked(RefreshOnPlayMenuItem, enabled);
+        }
+    }
+}
+
+#endif
---
+++ Editor/AssetsRefreshTool.cs
@@ -0,0 +1,61 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Build1.AssetsRefreshTool.Editor
+{
+    [InitializeOnLoad]
+    internal static class AssetsRefreshTool
+    {
+        public const string AutoRefreshKeyKey = "kAutoRefresh";
+        public const string RefreshOnPlayKey  = "Build1_AssetsRefreshTool_RefreshOnPlayEnabled";
+
+        static AssetsRefreshTool()
+        {
+            SetEnabled(GetEnabled(), false);
+        }
+
+        public static bool GetEnabled()
+        {
+            return EditorPrefs.GetBool(RefreshOnPlayKey);
+        }
+
+        public static bool SetEnabled(bool enabled, bool printToLog = true)
+        {
+            if (GetEnabled() == enabled)
+                return false;
+
+            EditorPrefs.SetBool(AutoRefreshKeyKey, !enabled);
+            EditorPrefs.SetBool(RefreshOnPlayKey, enabled);
+
+            if (enabled)
+                EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+            else
+                EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+
+            if (printToLog)
+            {
+                Debug.Log(enabled
+                              ? "AssetsRefreshTool: Refresh on Play enabled. Auto Refresh turned off."
+                              : "AssetsRefreshTool: Refresh on Play disabled. Auto Refresh turned on.");
+            }
+
+            return true;
+        }
+
+        /*
+         * Private.
+         */
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state != PlayModeStateChange.ExitingEditMode)
+                return;
+            Debug.Log("AssetsRefreshTool: Refreshing assets...");
+            AssetDatabase.Refresh(ImportAssetOptions.Default);
+        }
+    }
+}
+
+#endif
